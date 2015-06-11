7288166c03abb546798b24f38e638aa8701d7f49
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2015-06-11 15:19:09 +0200

    hello

--- TestProjGit/ViewController.h
+++ TestProjGit/ViewController.h
@@ -1,11 +1,3 @@
-//
-//  ViewController.h
-//  TestProjGit
-//
-//  Created by Alexander Yaremchuk on 6/11/15.
-//  Copyright (c) 2015 Alexander Yaremchuk. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
