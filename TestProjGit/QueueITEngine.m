bfe4fee1f4bde68c4cce842c021755c11e8aa195
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2015-06-24 10:21:12 +0200

    cleanup

--- TestProjGit/QueueITEngine.m
+++ TestProjGit/QueueITEngine.m
@@ -66,50 +66,50 @@
          userId:userId userAgent:userAgent
         appType:appType
         success:^(QueueStatus *queueStatus)
-     {
-         if (queueStatus.errorType != (id)[NSNull null])
-         {
-             if ([queueStatus.errorType isEqualToString:@"Configuration"])//ISSUE: when incorrect eventId is provided -> Runtime error type is returned instead of Config.
+        {
+             if (queueStatus.errorType != (id)[NSNull null])
              {
-                 NSException *e = [NSException exceptionWithName:@"ConfigurationException" reason:queueStatus.errorMessage userInfo:nil];
-                 @throw e;
+                 if ([queueStatus.errorType isEqualToString:@"Configuration"])//ISSUE: when incorrect eventId is provided -> Runtime error type is returned instead of Config.
+                 {
+                     NSException *e = [NSException exceptionWithName:@"ConfigurationException" reason:queueStatus.errorMessage userInfo:nil];
+                     @throw e;
+                 }
+                 if ([queueStatus.errorType isEqualToString:@"Runtime"])//ISSUE: when incorrect eventId is provided -> Runtime error type is returned instead of Config.
+                 {
+                     NSException *e = [NSException exceptionWithName:@"RuntimeException" reason:queueStatus.errorMessage userInfo:nil];
+                     @throw e;
+                 }
+                 //TODO: add 1 more else ifs for 1 more errorTypes and throw custom exceptions
              }
-             if ([queueStatus.errorType isEqualToString:@"Runtime"])//ISSUE: when incorrect eventId is provided -> Runtime error type is returned instead of Config.
+             
+             NSLog(@"queueUrl: %@, requeryInterval: %i", queueStatus.queueUrlString, queueStatus.requeryInterval);
+             
+             if (queueStatus.queueId != (id)[NSNull null] && queueStatus.queueUrlString == (id)[NSNull null] && queueStatus.requeryInterval == 0) //SafetyNet -> do nothing
              {
-                 NSException *e = [NSException exceptionWithName:@"RuntimeException" reason:queueStatus.errorMessage userInfo:nil];
-                 @throw e;
              }
-             //TODO: add 1 more else ifs for 1 more errorTypes and throw custom exceptions
-         }
-         
-         NSLog(@"queueUrl: %@, requeryInterval: %i", queueStatus.queueUrlString, queueStatus.requeryInterval);
-         
-         if (queueStatus.queueId != (id)[NSNull null] && queueStatus.queueUrlString == (id)[NSNull null] && queueStatus.requeryInterval == 0) //SafetyNet -> do nothing
-         {
-         }
-         else if (queueStatus.queueId != (id)[NSNull null] && queueStatus.queueUrlString != (id)[NSNull null] && queueStatus.requeryInterval == 0) //InQueue -> show queue page
-         {
-             [self showQueue:host queueUrl:queueStatus.queueUrlString customerId:customerId eventId:eventOrAliasId];
-             [self updateCache:queueStatus.queueUrlString urlTTL:queueStatus.queueUrlTTL customerId:customerId eventId:eventOrAliasId];
-         }
-         else if (queueStatus.queueId == (id)[NSNull null] && queueStatus.queueUrlString != (id)[NSNull null] && queueStatus.requeryInterval == 0) //Idle -> show queue page
-         {
-             [self showQueue:host queueUrl:queueStatus.queueUrlString customerId:customerId eventId:eventOrAliasId];
-         }
-         else if (queueStatus.requeryInterval > 0) //DisableEvent case -> continue polling at requeryInterval
-         {
-             dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
-                 [NSThread sleepForTimeInterval:queueStatus.requeryInterval];
-                 dispatch_async(dispatch_get_main_queue(), ^{
-                     [self tryEnqueue:host customerId:customerId eventOrAliasId:eventOrAliasId];
+             else if (queueStatus.queueId != (id)[NSNull null] && queueStatus.queueUrlString != (id)[NSNull null] && queueStatus.requeryInterval == 0) //InQueue -> show queue page
+             {
+                 [self showQueue:host queueUrl:queueStatus.queueUrlString customerId:customerId eventId:eventOrAliasId];
+                 [self updateCache:queueStatus.queueUrlString urlTTL:queueStatus.queueUrlTTL customerId:customerId eventId:eventOrAliasId];
+             }
+             else if (queueStatus.queueId == (id)[NSNull null] && queueStatus.queueUrlString != (id)[NSNull null] && queueStatus.requeryInterval == 0) //Idle -> show queue page
+             {
+                 [self showQueue:host queueUrl:queueStatus.queueUrlString customerId:customerId eventId:eventOrAliasId];
+             }
+             else if (queueStatus.requeryInterval > 0) //DisableEvent case -> continue polling at requeryInterval
+             {
+                 dispatch_async( dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                     [NSThread sleepForTimeInterval:queueStatus.requeryInterval];
+                     dispatch_async(dispatch_get_main_queue(), ^{
+                         [self tryEnqueue:host customerId:customerId eventOrAliasId:eventOrAliasId];
+                     });
                  });
-             });
-         }
-     }
+             }
+        }
         failure:^(NSError *error)
-     {
+        {
          //TODO: for any not 200 http status codes throw custom exception
-     }];
+        }];
 }
 
 -(void)updateCache:(NSString*)queueUrl urlTTL:(int)queueUrlTTL customerId:(NSString*)customerId eventId:(NSString*)eventId{
