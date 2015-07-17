57b6f88c7883e97da90b270b70d045ce4bad2c99
Alexander Yaremchuk <queut-it@Alexanders-Mac-mini.local>
2015-07-17 17:46:16 +0200

    fixes

--- TestProjGit/TopsTableViewController.m
+++ TestProjGit/TopsTableViewController.m
@@ -36,7 +36,8 @@
     NSString* layoutName = @"mobileios";
     NSString* language = @"en-US";
     
-    self.engine = [[QueueITEngine alloc]initWithHost:self customerId:customerId eventOrAliasId:eventAlias layoutName:layoutName language:language];
+    self.engine = [[QueueITEngine alloc]initWithHost:self customerId:customerId eventOrAliasId:eventAlias layoutName:nil language:nil];
+    //self.engine = [[QueueITEngine alloc]initWithHost:self customerId:customerId eventOrAliasId:eventAlias layoutName:layoutName language:language];
     self.engine.queuePassedDelegate = self;
     [self.engine run];
 }
